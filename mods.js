1bd4e0119c9bbc62d0066ff7f77dc26a7cfd9743
Chief7667 <xsaadahmad76@gmail.com>
2026-01-04 03:38:08 -0500

    Update modName and eBay link in mods.js

--- mods.js
+++ mods.js
@@ -13,10 +13,10 @@ window.MODS = [
   {
     image: "assets/placeholder.svg",
     videoTitle: "Cold start after install",
-    modName: "K&N Intake (example)",
+    modName: "Center Console Side Trims ",
     links: [
       { label: "Amazon", url: "https://example.com" },
-      { label: "eBay", url: "https://example.com" },
+      { label: "eBay", url: "https://www.ebay.com/itm/385342949332?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339136349&toolid=80005&mkevt=1&amdata=enc%3AAQAKAAAAoFu2GTm2ZGNVXFEsdeNYg5J4SWYbN3z%2FCsG9w2%2FXTGILD2BxalctyEw81iVc9iFuGaFbW7wlG1jHM1xF4zoRA289Gr0zMgB2AKifSjzf%2F0PT4%2FutjIMYAyRx27QOM7HwVIdAbW6xCUJRPNpeuclW8dg9rek5hviA%2BYf58Ay2sKbSolY%2BpwY6GUOef4cqtvVX6SsuEpdyQRSmp%2B9JPLXHX3A%3D" },
       { label: "AliExpress", url: "https://example.com" }
     ]
   },
